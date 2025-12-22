6846581272cd3db7c80064e41eb2a3b437805df4
AzDar-Vs <azhar.21038@mhs.unesa.ac.id>
2025-12-22 13:30:18 +0700

    Delete frontend directory

--- frontend/src/components/layout/dashboardLayout.jsx
+++
@@ -1,8 +0,0 @@
-const DashboardLayout = ({ sidebar, children }) => (
-  <div className="d-flex">
-    {sidebar}
-    <main className="p-4 w-100">{children}</main>
-  </div>
-);
-
-export default DashboardLayout;
--- frontend/src/services/categoryService.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-const categoryService = {
-  getGenres: () =>
-    api.get('/categories/genres')
-};
-
-export default categoryService;
