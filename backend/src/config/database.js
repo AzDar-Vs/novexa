e7453246256e38e48fd2f1b07cffe1505752949e
AzDar-Vs <azhar.21038@mhs.unesa.ac.id>
2025-12-22 14:54:39 +0700

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize');
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+const db = new Sequelize(process.env.MYSQL_URL, {
+  dialect: 'mysql',
+  logging: false
 });
 
 module.exports = db;
