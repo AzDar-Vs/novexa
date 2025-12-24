4a6218209c008da28b06935a4b9f78bd0a7db8d7
AzDar-Vs <azhar.21038@mhs.unesa.ac.id>
2025-12-24 22:31:44 +0700

    Delete frontend directory

--- frontend/src/api/reader.api.js
+++
@@ -1,7 +0,0 @@
-import api from './axios';
-
-export const getChaptersApi = (bookId) =>
-  api.get(`/reader/book/${bookId}`);
-
-export const readChapterApi = (babId) =>
-  api.get(`/reader/chapter/${babId}`);
